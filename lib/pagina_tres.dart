109e72a1d6f2f100eaae8c37368e565ae1cf7fbe
LUIS_CESAR_CERVANTES_VELAZQUEZ <a.22308051281057@cbtis128.edu.mx>
2025-04-08 17:52:28 +0000

    ahora si pantallas

---
+++ lib/pagina_tres.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PantallaTres extends StatelessWidget {
+  const PantallaTres({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff368c57), // Fondo azul
+        title: const Text(
+          'Pantalla Tres 3',
+          style: TextStyle(
+            color: Colors.black, // Letra blanca
+            fontSize: 20.0, // Tamaño de la letra 20
+          ),
+        ),
+        centerTitle: true, // Centrar el texto
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Pantalla Inicial MiChavo'),
+        ),
+      ),
+    );
+  }
+}
